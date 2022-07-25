a2de71ffe5cb00ee466dcb3465b6be032df68229
Youssef Mahmoud <youssef@noreply.github.com>
2022-07-25 19:37:57 +0200

    auto encrypt password, make users seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         \App\Models\User::factory(2)->create();
         School::factory(3)->create();
         Student::factory(10)->create();
     }
