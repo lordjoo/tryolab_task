b80008a0e243fd73d7ff35adebb15b4a3dcf15d1
Youssef Mahmoud <youssef.afify122@gmail.com>
2022-07-26 18:31:19 +0200

    update test

--- tests/Feature/StudentApiResourceTest.php
+++ tests/Feature/StudentApiResourceTest.php
@@ -10,23 +10,31 @@ use Tests\TestCase;
 
 class StudentApiResourceTest extends TestCase
 {
-//    use RefreshDatabase;
+    use RefreshDatabase;
 
-    public function test_resource() {
-        $user = User::factory()->make();
-        $user->save();
-        $user->refresh();
+    private mixed $token;
+
+    public function set_token()
+    {
+        $user = User::factory()->create();
         $token = $this->postJson('/api/auth/login', [
             'email' => $user->email,
             'password' => 'password'
         ]);
         $token->assertStatus(200)->assertJsonPath('data.token', $token->json()['data']['token']);
-        $token = $token->json()["data"]['token'];
-
+        $this->token = $token->json()["data"]['token'];
         $this->assertNotEmpty($token);
-        $this->withHeader('Authorization', 'Bearer ' . $token);
-        $this->getJson('/api/students')->assertStatus(200);
+        $this->withHeader('Authorization', 'Bearer ' . $this->token);
+    }
 
+    public function test_get_students_endpoint_return_status_ok()
+    {
+        $this->set_token();
+        $this->getJson('/api/students')->assertStatus(200);
+    }
+
+    public function test_create_and_edit_students_endpoint_return_status_ok() {
+        $this->set_token();
         $student = Student::factory()->make();
         $create_student = $this->postJson('/api/students', [
             'name' => $student->name,
@@ -34,15 +42,17 @@ class StudentApiResourceTest extends TestCase
         ]);
         $create_student->assertStatus(200)->assertJsonPath('data.id', $create_student->json()['data']['id']);
 
-        // try update student
         $update_student = $this->putJson('/api/students/' . $create_student->json()['data']['id'], [
             'name' => $student->name,
             "school_id" => $student->school_id,
         ]);
         $update_student->assertStatus(200)->assertJsonPath('data.id', $update_student->json()['data']['id']);
 
-        // try to delete student
-        $delete_student = $this->deleteJson('/api/students/' . $create_student->json()['data']['id']);
+    }
+
+    public function test_delete_student_endpoint_return_status_ok() {
+        $this->set_token();
+        $delete_student = $this->deleteJson('/api/students/' . Student::factory()->create()->id);
         $delete_student->assertStatus(200);
     }
 
